848b2633878c34a6b3593e8f0a3c1c29cd4f4f9d
Danvopros <127744934+Danvopros@users.noreply.github.com>
2023-03-13 14:47:54 +0300

    Add files via upload

--- LR20/Program.cs
+++ LR20/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Таблица умножения");
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write($"{i * j}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Таблица сложения");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($"{i + j}\t");
+                }
+                 Console.WriteLine();
+                
+            }
+        }
+    }
+}
